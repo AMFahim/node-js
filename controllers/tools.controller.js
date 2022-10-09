7201fa14afe8e57ee627b783457402d369092559
AMFahim <amfahim2019@gmail.com>
2022-10-09 10:04:07 +0600

    Handeling get request

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const { getDb } = require("../utils/dbConnect")
 
 let tools = [
@@ -18,38 +19,59 @@ let tools = [
   },
 ]
 
-module.exports.getAllTools = (req, res, next) => {
-  res.json(tools)
+module.exports.getAllTools = async (req, res, next) => {
+
+  try {
+    const {limit, page} = req.query;
+    const db = getDb();
+
+    const tool = await db
+      .collection("clickwrk")
+      .find({})
+      // .project({ _id: 0 })
+      .skip(+page * limit)
+      .limit(+limit)
+      .toArray();
+
+    res.status(200).json({ success: true, data: tool })
+  } catch (error) {
+    next(error)
+  }
+
+
+  // res.json(tools)
 }
 
-module.exports.savaATools = async(req, res, next) => {
+module.exports.savaATools = async (req, res, next) => {
   try {
     const db = getDb();
     const tool = req.body;
 
     const result = await db.collection("clickwrk").insertOne(tool);
-    if(!result.insertedId) {
-      return res.status(400).send({status: false, error: "Something Went Wrong"})
+    if (!result.insertedId) {
+      return res.status(400).send({ status: false, error: "Something Went Wrong" })
     }
-    res.send(`Tool added with id: ${result.insertedId}`)
+    res.send({ success: true, message: `Tool added with id: ${result.insertedId}` })
   } catch (error) {
     next(error)
   }
 }
 
-module.exports.getToolDetail = (req, res) => {
-  const { id } = req.params
-  console.log(id);
-  const foundTool = tools.find(tool => tool.id === Number(id))
-  res.status(200).send({
-    success: true,
-    message: "Success",
-    data: foundTool
-  })
-  // res.status(500).send({
-  //   success: false,
-  //   error: "Internal Server Error!"
-  // })
+module.exports.getToolDetail = async (req, res, next) => {
+  try {
+    const db = getDb();
+    const {id} = req.params;
+
+    if(!ObjectId.isValid(id)) {
+      return res.status(400).json({success: false, error: "Not a valid tool id"})
+    }
+
+    const tool = await db.collection("clickwrk").findOne({_id: ObjectId(id)});
+    res.status(200).json({success: true, data: tool});
+
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports.updateTool = (req, res) => {
