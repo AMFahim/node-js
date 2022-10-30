a3d3395a2cb0521892a27f29e1e5a1055ff33f35
AMFahim <amfahim2019@gmail.com>
2022-10-30 23:23:49 +0600

    invetory app: Create Model and Insert Data

---
+++ app.js
@@ -0,0 +1,124 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose  = require("mongoose");
+
+// middleware 
+app.use(express.json());
+app.use(cors());
+
+
+// Schema design 
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please provide a name for this product"],
+    trim: true,
+    unique: [true, "Name must be unique"],
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: [100, "Name is too large."]
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price can't be negative"],
+  },
+  unit: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["kg", "liter", "pcs"],
+      message: "unit value can't be {VALUE}, must be kg/liter/pcs"
+    }
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: [0, "Quantity cant be negative"],
+    validate: {
+      validator: (value) => {
+        const isInteger = Number.isInteger(value);
+        if(isInteger) {
+          return true
+        } else {
+          return false
+        }
+      }
+    },
+    message: "quantity must be an integer"
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["in-stock", "out-of-stock", "discontinued"],
+      message: "Status can't be {VALUE}"
+    }
+  },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now
+  // },
+  // updatedAt: {
+  //   type: Date,
+  //   default: Date.now
+  // }
+
+  supplier: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Supplier"
+  },
+  categories: [{
+    name: {
+      type: String,
+      required: true
+    },
+    _id: mongoose.Schema.Types.ObjectId
+  }]
+}, {timeStamps: true})
+
+//SCHEMA --> MODEL --> QUERY
+
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get("/", (req, res) => {
+  res.send("Route is working! YaY!");
+});
+
+//posting to database
+
+app.post('/api/v1/product', async(req, res, next) => {
+  // res.send("It is working")
+
+  try {
+
+    // create 
+    // const result = await Product.create(req.body)
+
+     //Save
+    const product = new Product(req.body);
+
+    const result = await product.save();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Data inserted successfully",
+      data: result
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: "Fail",
+      message: "Data didn't saved",
+      data: error.message
+    })
+  }
+})
+
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const colors = require("colors");
+// const DBConnect = require("./utils/dbConnect");
+
+const app = require("./app");
+
+// database connection
+// DBConnect();
+
+mongoose.connect(process.env.DATABASE).then(() => {
+  console.log(`Database connection is successful`.red.bold)
+})
+
+// server
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`.yellow.bold);
+});
+
